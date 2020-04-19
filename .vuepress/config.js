62e4da111a443eaac3f7fd43d8d7e2542835725f
zhizxs <cxqstyle@gmail.com>
2020-04-19 23:02:51 +0800

    add seo

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,6 +10,32 @@ module.exports = {
         content: "width=device-width,initial-scale=1,user-scalable=no",
       },
     ],
+    ["meta", { name: "author", content: "zhiz" }],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "江湖夜雨,vuepress,reco,vuepress-reco,vuepress-theme-reco,blog,个人博客,前端开发,前端,javascript",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#42b983" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    ["link", { rel: "apple-touch-icon", href: "/icon_vuepress_reco.png" }],
+    [
+      "link",
+      { rel: "mask-icon", href: "/icon_vuepress_reco.svg", color: "#42b983" },
+    ],
+    [
+      "meta",
+      { name: "msapplication-TileImage", content: "/icon_vuepress_reco.png" },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
   ],
   theme: "reco",
   themeConfig: {
